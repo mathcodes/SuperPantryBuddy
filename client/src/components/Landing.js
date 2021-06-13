7b9565d04899a7d5998091df949f613d5760197a
fullstack11235 <fullstack11235@gmail.com>
2021-06-13 18:52:24 -0400

    pantry image

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { useAuth0 } from "@auth0/auth0-react";
 import { Link } from "react-router-dom";
+import pantry from "../assets/img/isoPantry.png"
 
 export default () => {
     const { loginWithRedirect } = useAuth0();
@@ -15,6 +16,9 @@ export default () => {
                             className="fas fa-sign-in-alt tabicon"
                             aria-hidden="true"></i> on the bottom right of the screen to login and get started!!!
             </h2>
+            <br/ >
+            <img src={pantry} alt="pantry image" height={"50%"}></img>
+            
         </>
     );
 };
